a750925800164c66ab0fdf593be63714eef8783a
PPODO <sherlockwhooo@gmail.com>
2021-01-24 21:35:19 +0900

    Update

--- Source/Locomotion/LocomotionCharacter.h
+++ Source/Locomotion/LocomotionCharacter.h
@@ -26,9 +26,38 @@ private:
 	void PlayerMovementInput(bool bIsForwardInput);
 
 #pragma region essential information
+private:
+	void SetEssentialValues();
+
+	FVector CalculateAcceleration();
+
 public:
 	void GetEssentialValues(const FVector& vVelocity, const FVector& vAcceleration, const FVector& vMovementInput, bool bIsMoving, bool bHasMovementInput, float fSpeed, const FRotator& rAimRotation, float fAimYawRate);
 	
+private:
+	FVector m_vAcceleration;
+	
+	bool m_bIsMoving;
+	bool m_bHasMovementInput;
+
+	float m_fSpeed;
+	float m_fMovementInputAmount;
+	float m_fAimYawRate;
+
+	FRotator m_rLastVelocityRotation;
+	FRotator m_rLastMovementInputRotation;
+
+#pragma endregion
+#pragma region cache values
+private:
+	void CacheValues();
+
+private:
+	FVector m_vPreviousVelocity;
+	float m_fPreviousAimYaw;
+
+#pragma endregion
+#pragma region state values
 
 #pragma endregion
 
