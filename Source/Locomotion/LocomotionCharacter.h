468dce01b1208cf1f1013058f863e53aae9af97d
PPODO <sherlockwhooo@gmail.com>
2021-01-27 22:00:03 +0900

    Update

--- Source/Locomotion/LocomotionCharacter.h
+++ Source/Locomotion/LocomotionCharacter.h
@@ -2,7 +2,42 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
+#include <Locomotion/AdvancedLocomotionAnimInstance.h>
+#include <Curves/CurveVector.h>
 #include "LocomotionCharacter.generated.h"
+
+USTRUCT(BlueprintType)
+struct FFMovementSetting {
+	GENERATED_BODY()
+public:
+	UPROPERTY(BlueprintReadWrite)
+		float WalkSpeed;
+	UPROPERTY(BlueprintReadWrite)
+		float RunSpeed;
+	UPROPERTY(BlueprintReadWrite)
+		float SprintSpeed;
+
+	UPROPERTY(BlueprintReadWrite)
+		class UCurveVector* MovementCurve;
+	UPROPERTY(BlueprintReadWrite)
+		class UCurveFloat* RotationRateCurve;
+
+public:
+	FFMovementSetting() : WalkSpeed(0.f), RunSpeed(0.f), SprintSpeed(0.f), RotationRateCurve(nullptr), MovementCurve(nullptr) {};
+	FFMovementSetting(float fWalkSpeed, float fRunSpeed, float fSprintSpeed, class UCurveFloat* rotationCurve, class UCurveVector* movementCurve) 
+		: WalkSpeed(fWalkSpeed), RunSpeed(fRunSpeed), SprintSpeed(fSprintSpeed), RotationRateCurve(rotationCurve), MovementCurve(movementCurve) {};
+	FFMovementSetting(float fWalkSpeed, float fRunSpeed, float fSprintSpeed, FString rotationCurveAssetPath, FString movementCurveAssetPath)
+		: WalkSpeed(fWalkSpeed), RunSpeed(fRunSpeed), SprintSpeed(fSprintSpeed) {
+		ConstructorHelpers::FObjectFinder<class UCurveFloat> rotationCurveFloat(*rotationCurveAssetPath);
+		ConstructorHelpers::FObjectFinder<class UCurveVector> movementCurveVector(*movementCurveAssetPath);
+
+		if (rotationCurveFloat.Succeeded())
+			RotationRateCurve = rotationCurveFloat.Object;
+		if (movementCurveVector.Succeeded())
+			MovementCurve = movementCurveVector.Object;
+	};
+
+};
 
 UCLASS()
 class LOCOMOTION_API ALocomotionCharacter : public ACharacter {
@@ -32,7 +67,7 @@ private:
 	FVector CalculateAcceleration();
 
 public:
-	void GetEssentialValues(const FVector& vVelocity, const FVector& vAcceleration, const FVector& vMovementInput, bool bIsMoving, bool bHasMovementInput, float fSpeed, const FRotator& rAimRotation, float fAimYawRate);
+	void GetEssentialValues(FVector& vVelocity, FVector& vAcceleration, FVector& vMovementInput, bool& bIsMoving, bool& bHasMovementInput, float& fSpeed, FRotator& rAimRotation, float& fAimYawRate);
 	
 private:
 	FVector m_vAcceleration;
@@ -57,7 +92,32 @@ private:
 	float m_fPreviousAimYaw;
 
 #pragma endregion
+#pragma region movement system
+private:
+	void UpdateCharacterMovement();
+	void UpdateDynamicMovementSetting(ECharacterGait allowedGait);
+
+	bool CanSprint();
+
+	FFMovementSetting GetTargetMovementSetting();
+
+private:
+	FFMovementSetting m_currentMovementSetting;
+
 #pragma region state values
+private:
+	ECharacterGait GetAllowedGait();
+	ECharacterGait GetActualGait(ECharacterGait allowedGait);
+
+private:
+	ERotationMode m_rotationMode;
+	ECharacterGait m_gait;
+
+#pragma endregion
+#pragma endregion
+#pragma region input
+private:
+	ECharacterGait m_desiredGait;
 
 #pragma endregion
 
